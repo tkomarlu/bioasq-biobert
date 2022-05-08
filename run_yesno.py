bdb77e0d08a3b8769d6b937acd138c249a244415
Tanay Komarlu <54658599+tkomarlu@users.noreply.github.com>
2022-05-07 22:24:45 -0500

    Update run_yesno.py

--- run_yesno.py
+++ run_yesno.py
@@ -714,8 +714,8 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
 
     assert len(prelim_predictions) == 1
     # Not used..
-    # probs = _compute_softmax(logits)
-    probs = logits
+    probs = _compute_softmax(logits)
+#     probs = logits
     for (i, entry) in enumerate(prelim_predictions):
       output = collections.OrderedDict()
       output["text"] = entry.answer
