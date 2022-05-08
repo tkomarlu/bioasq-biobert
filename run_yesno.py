8c9e0ffa9a53fd7c95bd94458f106ad2f1663937
Tanay Komarlu <54658599+tkomarlu@users.noreply.github.com>
2022-05-07 22:50:01 -0500

    Update run_yesno.py

--- run_yesno.py
+++ run_yesno.py
@@ -715,7 +715,6 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
     assert len(prelim_predictions) == 1
     # Not used..
     probs = _compute_sigmoid(logits[0])
-    print(logits)
 #     probs = logits
     for (i, entry) in enumerate(prelim_predictions):
       output = collections.OrderedDict()
